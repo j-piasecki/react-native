77187dd2067047581d0bc6302a761497ffb5b2c0
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-08-28 17:17:12 +0200

    example

--- packages/rn-tester/js/RNTesterAppShared.js
+++ packages/rn-tester/js/RNTesterAppShared.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { Button, Pressable, SafeAreaView, ScrollView, View } from 'react-native';
 
 export default function App() {
+  const [toggle, setToggle] = React.useState(false);
   return (
     <View style={{flex: 1, paddingTop: 100}}>
       <SafeAreaView style={{width: '100%', height: 200}}>
-        <View style={{flex: 1, backgroundColor: 'red'}} />
-        <View style={{flex: 2, backgroundColor: 'green'}} />
-        <View style={{display: 'flex', flexDirection: 'row', flex: 1}}>
-          {/* <View style={{display: 'contents'}}> */}
-            <View style={{flex: 1, backgroundColor: 'blue'}} />
-            <View style={{flex: 1, backgroundColor: 'cyan'}} />
-          {/* </View> */}
+        <Pressable style={{width: 100, height: 100, backgroundColor: 'black'}} onPress={() => setToggle(!toggle)}>
+          <ScrollView />
+        </Pressable>
+        <View style={{display: 'flex', flexDirection: 'row', flex: 1, backgroundColor: 'magenta'}}>
+          <SafeAreaView style={{
+            // display: 'contents',
+            flex: 1,
+            }}>
+            <View style={{
+              display: 'contents',
+              width: '100%',
+              height: 200,
+              }}>
+              <View style={{
+                  display: 'contents',
+                  flex: 1,
+                }}>
+                { toggle && <View style={{flex: 1, backgroundColor: 'yellow'}} /> }
+                <View style={{flex: 1, backgroundColor: 'blue'}} />
+                <View style={{flex: 1, backgroundColor: 'cyan'}} />
+              </View>
+            </View>
+          </SafeAreaView>
         </View>
       </SafeAreaView>
     </View>
