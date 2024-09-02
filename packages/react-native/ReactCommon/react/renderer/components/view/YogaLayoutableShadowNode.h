f6c84d6141d3438acb47299613646529af916262
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-09-02 10:26:00 +0200

    Don't build a children list many times

--- packages/react-native/ReactCommon/react/renderer/components/view/YogaLayoutableShadowNode.h
+++ packages/react-native/ReactCommon/react/renderer/components/view/YogaLayoutableShadowNode.h
@@ -115,13 +115,13 @@ class YogaLayoutableShadowNode : public LayoutableShadowNode {
    * -> back to owned because its parent is allocated at the same address.
    */
   void updateYogaChildrenOwnersIfNeeded();
-    
-  std::vector<Child> buildFlattenedChildrenList(const YogaLayoutableShadowNode::Shared& node) const;
+
+  void runForEveryConcreteSubtree(const YogaLayoutableShadowNode::Shared& node, std::function<void(const YogaLayoutableShadowNode::Shared& subtreeRoot)> fn) const;
 
   void initialize();
 
   void setContentsLayoutMetrics(LayoutContext layoutContext);
-    
+
   /*
    * Return true if child's yogaNode's owner is this->yogaNode_. Otherwise
    * returns false.
