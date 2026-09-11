ca2e11af51280611fe4740b8b472f4b29a826a7b
Christoph Purrer <chpurrer@meta.com>
2026-09-11 09:29:58 -0700

    Keep only release level on DefaultNewArchitectureEntryPoint (#58469)
    Summary:
    Pull Request resolved: https://github.com/react/react-native/pull/58469
    
    TurboModules, Fabric and bridgeless are all unconditionally enabled in the New
    Architecture, so every remaining flag on `DefaultNewArchitectureEntryPoint` was
    dead configuration. Reduce it to the one thing it still selects: the release
    level.
    
    Removes the deprecated parameterized `load(...)` overloads, the `fabricEnabled`,
    `turboModulesEnabled` and `concurrentReactEnabled` getters, and
    `isConfigurationValid`. Call sites that passed `fabricEnabled` to
    `DefaultReactActivityDelegate` now use its two-argument constructor, which
    already ignored the flag.
    
    Changelog:
    [Android][Breaking] - Remove the deprecated `DefaultNewArchitectureEntryPoint.load(turboModulesEnabled, fabricEnabled)` overloads; use `load()` instead
    [Android][Breaking] - Remove `DefaultNewArchitectureEntryPoint.fabricEnabled`, `turboModulesEnabled`, `concurrentReactEnabled` and `isConfigurationValid`
    
    Reviewed By: javache
    
    Differential Revision: D119370657
    
    fbshipit-source-id: 4964d5163af9bc1a74e3a4b9ae6933df98683e5f

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultNewArchitectureEntryPoint.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultNewArchitectureEntryPoint.kt
@@ -5,12 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-@file:Suppress("DEPRECATION") // We want to use ReactFeatureFlags here specifically
-
 package com.facebook.react.defaults
 
 import com.facebook.react.common.ReleaseLevel
-import com.facebook.react.common.annotations.VisibleForTesting
 import com.facebook.react.internal.featureflags.ReactNativeFeatureFlags
 import com.facebook.react.internal.featureflags.ReactNativeFeatureFlagsOverrides_RNOSS_Canary_Android
 import com.facebook.react.internal.featureflags.ReactNativeFeatureFlagsOverrides_RNOSS_Experimental_Android
@@ -20,61 +17,20 @@ import com.facebook.react.internal.featureflags.ReactNativeFeatureFlagsProvider
 /**
  * A utility class that serves as an entry point for users setup the New Architecture.
  *
- * This class needs to be invoked as `DefaultNewArchitectureEntryPoint.load(...)` by passing a
- * series of optional parameters.
+ * This class needs to be invoked as `DefaultNewArchitectureEntryPoint.load()`, optionally after
+ * setting [releaseLevel] to pick the set of feature flags to apply.
  *
  * By default it loads a library called `appmodules`. `appmodules` is a convention used to refer to
  * the application dynamic library. If changed here should be updated also inside the template.
- *
- * By default it also enables both TurboModules, Fabric and Concurrent React (aka React 18), and
- * Bridgeless
  */
 public object DefaultNewArchitectureEntryPoint {
 
   public var releaseLevel: ReleaseLevel = ReleaseLevel.STABLE
 
-  /**
-   * Loads the React Native New Architecture entry point with the default configuration.
-   *
-   * This will load the app with TurboModules, Fabric and Bridgeless by default.
-   */
+  /** Loads the React Native New Architecture entry point for the configured [releaseLevel]. */
   @JvmStatic
   public fun load() {
-    load(turboModulesEnabled = true, fabricEnabled = true)
-  }
-
-  @JvmStatic
-  @Deprecated(
-      message =
-          "Loading the entry point with different flags for Fabric and TurboModule is deprecated." +
-              " Please use load() instead when loading the New Architecture.",
-      replaceWith = ReplaceWith("load()"),
-  )
-  public fun load(
-      turboModulesEnabled: Boolean = true,
-  ) {
-    load(turboModulesEnabled, fabricEnabled = true)
-  }
-
-  @JvmStatic
-  @Deprecated(
-      message =
-          "Loading the entry point with different flags for Fabric and TurboModule is deprecated." +
-              " Please use load() instead when loading the New Architecture.",
-      replaceWith = ReplaceWith("load()"),
-  )
-  public fun load(
-      turboModulesEnabled: Boolean = true,
-      fabricEnabled: Boolean = true,
-  ) {
-    val (isValid, errorMessage) = isConfigurationValid(turboModulesEnabled, fabricEnabled)
-    if (!isValid) {
-      error(errorMessage)
-    }
-
     ReactNativeFeatureFlags.override(getDefaultFeatureFlagsProvider())
-
-    privateTurboModulesEnabled = turboModulesEnabled
 
     DefaultSoLoader.maybeLoadSoLibrary()
   }
@@ -82,8 +38,6 @@ public object DefaultNewArchitectureEntryPoint {
   @JvmStatic
   internal fun loadWithFeatureFlags(featureFlags: ReactNativeFeatureFlagsProvider) {
     ReactNativeFeatureFlags.override(featureFlags)
-
-    privateTurboModulesEnabled = true
 
     DefaultSoLoader.maybeLoadSoLibrary()
   }
@@ -93,34 +47,5 @@ public object DefaultNewArchitectureEntryPoint {
         ReleaseLevel.EXPERIMENTAL -> ReactNativeFeatureFlagsOverrides_RNOSS_Experimental_Android()
         ReleaseLevel.CANARY -> ReactNativeFeatureFlagsOverrides_RNOSS_Canary_Android()
         ReleaseLevel.STABLE -> ReactNativeFeatureFlagsOverrides_RNOSS_Stable_Android()
-      }
-
-  @JvmStatic
-  public val fabricEnabled: Boolean
-    get() = true
-
-  private var privateTurboModulesEnabled: Boolean = false
-
-  @JvmStatic
-  public val turboModulesEnabled: Boolean
-    get() = privateTurboModulesEnabled
-
-  @JvmStatic
-  public val concurrentReactEnabled: Boolean
-    get() = true
-
-  @VisibleForTesting
-  public fun isConfigurationValid(
-      turboModulesEnabled: Boolean,
-      fabricEnabled: Boolean,
-  ): Pair<Boolean, String> =
-      if (!turboModulesEnabled || !fabricEnabled) {
-        false to
-            "You cannot load React Native with the New Architecture disabled. " +
-                "Please use DefaultNewArchitectureEntryPoint.load() instead of " +
-                "DefaultNewArchitectureEntryPoint.load(turboModulesEnabled=$turboModulesEnabled, " +
-                "fabricEnabled=$fabricEnabled)"
-      } else {
-        true to ""
       }
 }
